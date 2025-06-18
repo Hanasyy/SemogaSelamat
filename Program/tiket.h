3aa20ffe374f09c1d5b73f1958a957f7696c44cb
auliaRachmad <aulia.rachmad21@gmail.com>
2025-06-18 09:09:24 +0700

    delete pemesanan.h in tiket.h

--- Program/tiket.h
+++ Program/tiket.h
@@ -4,8 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "tipe_data_global.h"     
-#include "pemesanan.h"            
+#include "tipe_data_global.h"                
 #define MAX 100
 
 typedef struct {
